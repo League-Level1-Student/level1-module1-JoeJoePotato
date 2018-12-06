8b17e9cd51ffa28f88c9109f9b8a4031227e7ebf
JoeJoePotato <league@WTS-iMac-11.attlocal.net>
2018-12-05 17:39:18 -0800

    binary converter

--- src/binaryconverter.java
+++ src/binaryconverter.java
@@ -6,15 +6,60 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-public class binaryconverter {
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+public class binaryconverter implements ActionListener {
 public static void main(String[] args) {
-	JFrame frame=new JFrame();
-	JPanel panel=new JPanel();
-	frame.add(panel);
-	JTextField answer = new JTextField(20);
-	
+
+	binaryconverter bc= new binaryconverter();
+	bc.setup();
 	
 
 	
 }
+JTextField answer = new JTextField(20);
+JButton button=new JButton();
+JLabel ladel=new JLabel();
+public void setup() {
+
+		JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	frame.add(panel);
+	frame.setVisible(true);
+	button.addActionListener(this);
+	panel.add(button);
+	panel.add(answer);
+	panel.add(ladel);
+	ladel.setText("Enter 8 bits of binary and then click the button.");
+	frame.pack();
+
+}
+String convert(String input) {
+    if(input.length() != 8){
+         JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+         return "-";
+    }
+    String binary = "[0-1]+";    //must contain numbers in the given range
+    if (!input.matches(binary)) {
+         JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+         return "-";
+    }
+    try {
+         int asciiValue = Integer.parseInt(input, 2);
+         char theLetter = (char) asciiValue;
+         return "" + theLetter;
+    } catch (Exception e) {
+         JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+         return "-";
+    }
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+   String ugh= answer.getText();
+   String last=convert(ugh);
+   ladel.setText(ugh+" in binary is "+last+" in ASCII");
+   
+   }
 }
